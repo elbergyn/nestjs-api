ddec89bd9ee63fa0511853f1ca6d9326c81dc6aa
elbergyn <elber_gyn@hotmail.com>
2021-11-12 18:40:35 -0300

    inicio

---
+++ src/transactions/entities/transaction.entity.ts
@@ -0,0 +1,50 @@
+import { prependListener } from 'process';
+import {
+  Model,
+  Table,
+  Column,
+  DataType,
+  PrimaryKey,
+} from 'sequelize-typescript';
+
+export enum TransactionCategory {
+  CATEGORY1 = 'category1',
+  CATEGORY2 = 'category2',
+}
+
+export enum TransactionType {
+  CREDIT = 'credit',
+  DEBIT = 'debit',
+}
+
+@Table({
+  tableName: 'transaction',
+  createdAt: 'created_at',
+  updatedAt: 'updated_at',
+})
+export class Transaction extends Model {
+  @PrimaryKey
+  @Column({ type: DataType.UUID, defaultValue: DataType.UUIDV4 })
+  id: string;
+
+  @Column({ allowNull: false })
+  payment_date: Date;
+
+  @Column({ allowNull: false })
+  name: string;
+
+  @Column({ allowNull: false })
+  description: string;
+
+  @Column({ allowNull: false })
+  category: TransactionCategory;
+
+  @Column({
+    allowNull: false,
+    type: DataType.DECIMAL({ precision: 10, scale: 2 }),
+  })
+  amount: number;
+
+  @Column({ allowNull: false })
+  type: TransactionType;
+}
